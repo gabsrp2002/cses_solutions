ce36658a0e59aa68d8feea05254dd39102e5ed95
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2023-06-01 00:43:53 -0300

    feat: add subarray sums II solution

---
+++ sorting_and_searching/subarray_sums_II.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define db          \
+    cout << "Debug" \
+         << "\n"
+#define ll long long
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, x;
+    cin >> n >> x;
+    vector<ll> s(n + 1, 0);
+    map<ll, int> prevsum;
+    for (int i = 1; i <= n; i++) {
+        cin >> s[i];
+        s[i] += s[i - 1];
+    }
+    ll ans = 0;
+    for (int i = 1; i <= n; i++) {
+        if (s[i] == x) ans++;
+        
+        ans += prevsum[s[i] - x];
+        prevsum[s[i]]++;
+    }
+
+    cout << ans << '\n';
+
+    return 0;
+}
