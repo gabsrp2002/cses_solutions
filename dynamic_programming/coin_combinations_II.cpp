28c65b883a5eeddc3692b540fd4ff15fda96327b
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2023-06-06 17:34:41 -0300

    feat: update coin combination II solution

--- dynamic_programming/coin_combinations_II.cpp
+++ dynamic_programming/coin_combinations_II.cpp
@@ -11,6 +11,7 @@ const int mod = 1e9 + 7;
 
 int n, x;
 int c[105];
+int dp[1000005];
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -21,8 +22,16 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> c[i];
     }
-    sort(c, c + n);
+    dp[0] = 1;
 
+    for (int j = 0; j < n; j++) {
+        for (int i = 1; i <= x; i++) {
+            if (i < c[j]) continue;
+            dp[i] = (dp[i] + dp[i - c[j]]) % mod;
+        }
+    }
+
+    cout << dp[x] << '\n';
 
 
     return 0;
