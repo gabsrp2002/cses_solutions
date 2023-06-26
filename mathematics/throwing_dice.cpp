dec03891e459885b4edcf0be56d0b1bfde9adcba
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2023-06-26 10:07:15 -0300

    feat: add throwing dice solution

---
+++ mathematics/throwing_dice.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+#include <vector>
+
+using namespace std;
+
+#define dbg(x) cout << #x << " = " << x << endl
+typedef long long ll;
+const int mod = 1e9 + 7;
+
+typedef vector<vector<int>> Matrix;
+
+Matrix mult(Matrix &a, Matrix &b) {
+    Matrix res = vector<vector<int>>(a.size(), vector<int>(b[0].size(), 0));
+
+    for (int i = 0; i < a.size(); i++) {
+        for (int j = 0; j < b[0].size(); j++) {
+            for (int k = 0; k < b.size(); k++) {
+                res[i][j] = (res[i][j] + 1ll * a[i][k] * b[k][j]) % mod;
+            }
+        }
+    }
+
+    return res;
+}
+
+Matrix pot(Matrix base, ll e) {
+    Matrix res = vector<vector<int>>(base.size(), vector<int>(base.size(), 0));
+    for (int i = 0; i < base.size(); i++) {
+        res[i][i] = 1;
+    }
+
+    while (e) {
+        if (e & 1) res = mult(res, base);
+        base = mult(base, base);
+        e >>= 1;
+    }
+
+    return res;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    ll n;
+    cin >> n;
+
+    Matrix base = {{1, 1, 1, 1, 1, 1}, {1, 0, 0, 0, 0, 0}, {0, 1, 0, 0, 0, 0},
+                   {0, 0, 1, 0, 0, 0}, {0, 0, 0, 1, 0, 0}, {0, 0, 0, 0, 1, 0}};
+
+    cout << pot(base, n)[0][0] << '\n';
+
+    return 0;
+}
