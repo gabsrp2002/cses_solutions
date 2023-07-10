b58ef163a2e38b5741285d192d8be2469a6980aa
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2023-07-10 15:50:57 -0300

    feat: add minimum euclidean distance solution

---
+++ geometry/minimum_euclidean_distance.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define dbg(x) cout << #x << " = " << x << endl
+typedef long long ll;
+const int mod = 1e9 + 7;
+
+struct pt {
+    ll x, y;
+};
+
+struct cmp_x {
+    bool operator()(const pt & a, const pt & b) const {
+        return a.x < b.x || (a.x == b.x && a.y < b.y);
+    }
+};
+
+struct cmp_y {
+    bool operator()(const pt & a, const pt & b) const {
+        return a.y < b.y;
+    }
+};
+
+int n;
+vector<pt> a;
+
+ll mindist;
+
+void upd_ans(const pt & a, const pt & b) {
+    ll dist = (a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y);
+    mindist = min(dist, mindist);
+}
+
+vector<pt> t;
+
+void rec(int l, int r) {
+    if (r - l <= 3) {
+        for (int i = l; i < r; ++i) {
+            for (int j = i + 1; j < r; ++j) {
+                upd_ans(a[i], a[j]);
+            }
+        }
+        sort(a.begin() + l, a.begin() + r, cmp_y());
+        return;
+    }
+
+    int m = (l + r) >> 1;
+    ll midx = a[m].x;
+    rec(l, m);
+    rec(m, r);
+
+    merge(a.begin() + l, a.begin() + m, a.begin() + m, a.begin() + r, t.begin(), cmp_y());
+    copy(t.begin(), t.begin() + r - l, a.begin() + l);
+
+    int tsz = 0;
+    for (int i = l; i < r; ++i) {
+        if (abs(a[i].x - midx) * abs(a[i].x - midx) < mindist) {
+            for (int j = tsz - 1; j >= 0 && a[i].y - t[j].y < mindist; --j)
+                upd_ans(a[i], t[j]);
+            t[tsz++] = a[i];
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        a.push_back({x , y});
+    }
+    t.resize(n);
+    sort(a.begin(), a.end(), cmp_x());
+    mindist = 9e18;
+    rec(0, n);
+
+    cout << mindist << '\n';
+
+    return 0;
+}
+
