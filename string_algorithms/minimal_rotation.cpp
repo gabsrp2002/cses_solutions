e640dd8fc54c906679107371409638716a22f57e
Gabriel Ribeiro Paiva <gabrielpaiva2002@gmail.com>
2023-07-12 09:45:45 -0300

    feat: add minimal rotation solution

---
+++ string_algorithms/minimal_rotation.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define dbg(x) cout << #x << " = " << x << endl
+typedef long long ll;
+const int mod = 1e9 + 7;
+
+// Using Lyndon factorization
+string min_cyclic_string(string s) {
+    s += s;
+    int n = s.size();
+    int i = 0, ans = 0;
+    while (i < n / 2) {
+        ans = i;
+        int j = i + 1, k = i;
+        while (j < n && s[k] <= s[j]) {
+            if (s[k] < s[j])
+                k = i;
+            else
+                k++;
+            j++;
+        }
+        while (i <= k)
+            i += j - k;
+    }
+    return s.substr(ans, n / 2);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    string s;
+    cin >> s;    
+    cout << min_cyclic_string(s) << '\n';
+
+    return 0;
+}
